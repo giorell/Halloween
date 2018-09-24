8da43fd9e986f0826ebfe2f6941856afdae99ffc
Giordany Orellana <giorell@gmail.com>
2018-09-24 10:32:35 -0700

    Initial Commit

---
+++ Halloween/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Halloween
+//
+//  Created by Giordany Orellana on 9/24/18.
+//  Copyright © 2018 Giordany Orellana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
