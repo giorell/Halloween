6d322d33768d328ead4ba7f7eebb7475e023a674
Giordany Orellana <giordanyorellana@giordanys-imac.home>
2019-01-16 14:56:47 -0800

    first commit

---
+++ Halloween/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Halloween
+//
+//  Created by Giordany Orellana on 9/25/18.
+//  Copyright © 2018 Giordany Orellana. All rights reserved.
+//
+
+import Foundation
---
+++ Halloween/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  Halloween
+//
+//  Created by Giordany Orellana on 9/25/18.
+//  Copyright © 2018 Giordany Orellana. All rights reserved.
+//
+
+import Foundation
